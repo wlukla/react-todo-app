ece958a051966e5df1e9d4171ae0058c4aba2fea
wlukla <vladislav.danilochkin@gmail.com>
2020-01-31 20:13:37 +0200

    feat: turn ItemStatusFilter to functional component

--- src/components/item-status-filter/item-status-filter.jsx
+++ src/components/item-status-filter/item-status-filter.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class ItemStatusFilter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.buttons = [
-      { name: 'all', label: 'All' },
-      { name: 'active', label: 'Active' },
-      { name: 'done', label: 'Done' },
-    ];
-    this.state = {}; // !!!
-  }
+const ItemStatusFilter = (props) => {
+  const buttons = [
+    { name: 'all', label: 'All' },
+    { name: 'active', label: 'Active' },
+    { name: 'done', label: 'Done' },
+  ];
 
-  render() {
-    const buttons = this.buttons.map(({ name, label }) => {
-      const { filter, onFilterChange } = this.props;
-      const classNames = name === filter ? 'btn btn-primary' : 'btn btn-outline-secondary';
-      return (
-        <button
-          type="button"
-          className={classNames}
-          key={name}
-          onClick={() => onFilterChange(name)}
-        >
-          {label}
-        </button>
-      );
-    });
+  const buttonsDisplay = buttons.map(({ name, label }) => {
+    const { filter, onFilterChange } = props;
+    const classNames = name === filter ? 'btn btn-primary' : 'btn btn-outline-secondary';
     return (
-      <div className="btn-group">
-        {buttons}
-      </div>
+      <button
+        type="button"
+        className={classNames}
+        key={name}
+        onClick={() => onFilterChange(name)}
+      >
+        {label}
+      </button>
     );
-  }
-}
+  });
+
+  return (
+    <div className="btn-group">
+      {buttonsDisplay}
+    </div>
+  );
+};
 
 ItemStatusFilter.propTypes = {
   filter: PropTypes.string.isRequired,
   onFilterChange: PropTypes.func.isRequired,
 };
+
+export default ItemStatusFilter;
